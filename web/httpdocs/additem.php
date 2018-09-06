b41af04f778c63a63489f5924cdb1b9b2325b21d
Salisa <salizahahaha@gmail.com>
2018-09-07 03:10:12 +0700

    no message

---
+++ web/httpdocs/additem.php
@@ -0,0 +1,24 @@
+<?php
+    require 'index.php';
+    $item_model = $_POST['item_model'];
+    $item_name = $_POST['item_name'];
+    $item_description = $_POST['item_description'];
+    $option_id = $_POST['option_id'];
+    $price = $_POST['price'];
+    $priceperday =$_POST['priceperday'];
+    $deposit_1 = $_POST['deposit_1'];
+    $deposit_2 =$_POST['deposit_2'];
+    $type_id = $_POST['type_id'];
+    $brand_id = $_POST['brand_id'];
+    $status_id =$_POST['status_id'];
+    
+   $q = "INSERT INTO items(item_id,item_model,item_name,item_description,option_id,price,priceperday,deposit_1,deposit_2,type_id,brand_id,status_id) VALUES ('','$item_model','$item_name','$item_description','$option_id','$price','$priceperday','$deposit_1','$deposit_2','$type_id','$brand_id','$status_id')";
+   $result = mysqli_query($con, $q);
+   if($result){
+   echo "เพิ่มข้อมูลเรียบร้อย";}
+else{
+    echo "เกิดข้อผิดพลาด".mysqli_error($con);
+   
+   }
+   mysqli_close($con);
+
