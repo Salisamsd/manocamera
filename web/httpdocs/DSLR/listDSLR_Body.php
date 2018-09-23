4674f149edd7e6209b170102ed7fb0ee523cead4
Salisa <salizahahaha@gmail.com>
2018-09-23 14:30:27 +0700

    no message

--- web/httpdocs/DSLR/listDSLR_Body.php
+++ web/httpdocs/DSLR/listDSLR_Body.php
@@ -5,7 +5,7 @@
         <h3 align="left"> CANON  
    
             <a class="btn btn-info " href="insertBody.php" role="button"><img  class="icon" src="../img/IKONS/PNG/32/pen_1.png"> เพิ่ม </a><br>
-        <a class="btn btn-info " href="/2test.php" role="button"><img  class="icon" src="../img/IKONS/PNG/32/pen_1.png"> เพิ่ม </a>
+            <a class="btn btn-info " href="listDSLR_Body.php" role="button"><img  class="icon" src="../img/IKONS/PNG/32/pen_1.png"> เพิ่ม </a>
         </h3>
         
         <?php include('dslr_body.php');?>
