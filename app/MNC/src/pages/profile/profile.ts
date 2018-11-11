49cd126fc6b49dd7372db0c1c786b7b7706c1340
Salisa <salizahahaha@gmail.com>
2018-11-12 05:38:30 +0700

    no message

--- app/MNC/src/pages/profile/profile.ts
+++ app/MNC/src/pages/profile/profile.ts
@@ -35,21 +35,20 @@ oldEmailValue:any;
 @ViewChild("newtelephone")newtelephone;
 @ViewChild("newemail")newemail;
   constructor(public navCtrl: NavController, public alertCtrl: AlertController, public navParams: NavParams,  private http: Http,  public loading: LoadingController) {
-  }
-  ngOnInit(){
+  this.telephone = this.navParams.get('telephone') ;
+
+    this.email = this.navParams.get('email') ;}
+
+  Edit(){
+    
     this.username = this.navParams.get('username');
     
-    this.telephone = this.navParams.get('telephone') ;
-
-    this.email = this.navParams.get('email') ;
+    
 
     this.oldTelephoneValue = this.navParams.get('telephone') ;
 
     this.oldEmailValue = this.navParams.get('email') ;
 
-  }
-
-  Edit(){
     if(this.newtelephone.value=="" ){
 
       let alert = this.alertCtrl.create({
@@ -90,7 +89,8 @@ oldEmailValue:any;
       let options = new RequestOptions({ headers: headers });
  
     let data = {
- 
+    
+          username : this.username,
           telephone: this.oldTelephoneValue,
  
           email: this.oldEmailValue,
@@ -117,7 +117,7 @@ oldEmailValue:any;
  
    loader.dismiss()
  
-  if(res=='data updated successfully'){
+  if(res=='data update successfull'){
  
     let alert = this.alertCtrl.create({
  
@@ -131,7 +131,7 @@ oldEmailValue:any;
  
       alert.present();
  
-   this.navCtrl.push(ContactPage);
+   //this.navCtrl.push(ContactPage);
  
   }else
  
@@ -155,5 +155,14 @@ oldEmailValue:any;
 });
      }
     }
+    doRefresh(refresher) {
+      console.log('Begin async operation', refresher);
+  
+      setTimeout(() => {
+        console.log('Async operation has ended');
+        refresher.complete();
+      }, 2000);
+    }
   }
+  
  
