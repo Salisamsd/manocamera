3ef57edc0e8f303d40414f064642dbb6168e6cbe
Salisa <salizahahaha@gmail.com>
2018-11-25 16:28:19 +0700

    เทส upรูป

---
+++ app/11/src/pages/contact/contact.ts
@@ -0,0 +1,97 @@
+import { Component,ViewChild } from '@angular/core';
+import { NavController , ModalController , NavParams } from 'ionic-angular';
+import {Http, Headers, RequestOptions}  from "@angular/http";
+import { LoadingController } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+import { CartPage } from '../cart/cart';
+import { UpimagePage } from '../upimage/upimage';
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'contact.html'
+})
+export class ContactPage {
+  data:any;
+  username:any;
+  items:any;
+  telephone:any;
+  email:any;
+    constructor(public navCtrl: NavController, public navParams: NavParams,  private http: Http,  public loading: LoadingController,public modalCtrl: ModalController) {
+      this.username = this.navParams.get('username');
+      this.username = navParams.data;
+    }
+
+    ionViewDidLoad() {
+  
+      console.log('ionViewDidLoad ContactPage');
+      console.log(this.navParams.data);
+    }
+
+    
+    
+    ngOnInit(){
+  
+      this.username = this.navParams.get('username');
+  
+     
+  
+  var headers = new Headers();
+  
+  headers.append("Accept", 'application/json');
+  
+  headers.append('Content-Type', 'application/json' );
+  
+  let options = new RequestOptions({ headers: headers });
+  
+  let data = {
+  
+      username: this.username
+  
+       };
+  
+  let loader = this.loading.create({
+  
+  content: 'Processing please wait...',
+  
+  });
+  
+  loader.present().then(() => {
+  
+  this.http.post('http://manocamera.com/retrieve_data.php',data, options)
+  
+  .map(res => res.json())
+  
+      .subscribe(res => {
+  
+       loader.dismiss()
+  
+      this.items=res.server_response;
+  
+      console.log(this.items);
+  
+      });
+  
+      });
+  
+       }
+  
+       editPost(item){
+
+        this.navCtrl.push(ProfilePage,item)
+        
+          }
+
+          upPost(item){
+
+            this.navCtrl.push(UpimagePage,item)
+            
+              }
+          openCart(){
+            let data = {
+  
+              username: this.username
+          
+               };
+            
+      this.modalCtrl.create(CartPage,data).present();
+    }
+}
